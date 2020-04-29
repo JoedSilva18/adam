e4279205306f7543b4bcd75c46d3b784e0fc0ac7
Joed Silva <joedsilva2016@gmail.com>
2020-04-28 21:59:52 -0300

    add profile

--- dist/app/controllers/ColorController.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class ColorController {
-  async index(req, res) {
-    return res.status(200).json({
-      result: 'As cores disponiveis são: Azul, verde, vermelho',
-    });
-  }
-}
-
-exports. default = new ColorController();
