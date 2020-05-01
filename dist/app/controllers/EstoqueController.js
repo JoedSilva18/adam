5f8ed44dc618718fd04469e8552fdc36fcab4e7c
Joed Silva <joedsilva2016@gmail.com>
2020-05-01 15:22:49 -0300

    alterando rota de estoque

--- dist/app/controllers/EstoqueController.js
+++ dist/app/controllers/EstoqueController.js
@@ -2,8 +2,8 @@
   // Funcao busca no banco se o produto ainda tem em estoque
   async show(req, res) {
     const { productId, userId } = req.params;
-    const [, productId_] = productId.splice(':');
-    const [, userId_] = userId.splice(':');
+    const [, productId_] = productId.split(':');
+    const [, userId_] = userId.split(':');
 
     return res.status(200).json({
       result: `Ola, tudo bem? Ainda temos o produto em estoque mas não sabemos por quanto tempo! ${productId_} e ${userId_}`,
